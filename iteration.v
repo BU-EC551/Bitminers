373d20a043e8a0c5fc43a155f5c834607c309538
mars20 <marcia93@bu.edu>
2015-04-11 11:10:52 -0400

    Update iteration.v

--- iteration.v
+++ iteration.v
@@ -18,14 +18,16 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module iteration(t1_out, t2_out, a,b,c,d,e,f,g,h,message_in,select);
-output [31:0] reg t1_out, t2_out;	
+module iteration(t1_out, t2_out, a,b,c,d,e,f,g,h,message_in,select, clk);
+
+output  reg [31:0]t1_out, t2_out;	
 input [31:0] a,b,c,d,e,f,g,h,message_in;	
 input [5:0] select;
+input clk;
 reg [31:0] K[63:0];
 reg [31:0] message_array [63:0];
-wire [31:0]t1,t2, W;
-
+reg [31:0]t1,t2;
+reg [31:0] temp1,temp2;
 initial
 begin
 $readmemh("Kvalues.bin",K,0,63);
@@ -33,23 +35,29 @@ end
 
 	
 
-always @(select[0])
+always @(posedge clk)
 begin
+
+temp1= K[select-1];
+
 
 if (select < 16)
 		begin
-			W = message_in;
+	//W = message_in;
+			message_array[select] = message_in;
 		end
 	else 	
 		begin
-			W = (({message_array[select-1][16:0],message_array[select-1][31:17]}) ^ ({message_array[select-1][17:0], message_array[select-1][31:18]})^(message_array[select-1]>>3)) + message_array[select-6] + (({message_array[select-14][6:0], message_array[select-14][31:7]})^({message_array[select-14][17:0], message_array[select-14][31:18]})^(message_array[select-14]>>3)) + message_array[select-15] ;		
+			//W = (({message_array[select-2][16:0],message_array[select-2][31:17]}) ^ ({message_array[select-2][17:0], message_array[select-2][31:18]})^(message_array[select-2]>>3)) + message_array[select-7] + (({message_array[select-15][6:0], message_array[select-15][31:7]})^({message_array[select-15][17:0], message_array[select-15][31:18]})^(message_array[select-15]>>3)) + message_array[select-16] ;		
+			message_array[select] = (({message_array[select-2][16:0],message_array[select-2][31:17]}) ^ ({message_array[select-2][17:0], message_array[select-2][31:18]})^(message_array[select-2]>>3)) + message_array[select-7] + (({message_array[select-15][6:0], message_array[select-15][31:7]})^({message_array[select-15][17:0], message_array[select-15][31:18]})^(message_array[select-15]>>3)) + message_array[select-16] ;;
 		end
-message_array[select] = message_in;
-t1=h+({e[5:0], e[31:6]})^({e[10:0], e[31:11]})^({e[24:0], e[31:25]})+(e&f)^((~e)&g)+K[select]+W;
-t2=({a[1:0], a[31:2]})^({a[12:0], a[31:13]})^({a[21:0], a[31:22]})+(a&b)^(a&c)^(b&c);
+temp2 = message_array[select-1];
+t1=h+(({e[5:0], e[31:6]})^({e[10:0], e[31:11]})^({e[24:0], e[31:25]}))+((e&f)^((~e)&g))+temp1+ temp2;
+t2=(({a[1:0], a[31:2]})^({a[12:0], a[31:13]})^({a[21:0], a[31:22]}))+((a&b)^(a&c)^(b&c));
 t1_out = t1+t2;
-t2_out = d+t2;
+t2_out = d+t1;
 
+end
 
 
 
