a6b0b8392ded0f67f7e419748e747a6294f1a1d7
Priya Baskaran <priyadgopal@gmail.com>
2015-04-17 10:48:15 -0400

    Transmit and receive chunks of bytes controlled by keyinput - For demo

--- windows_serial_port_comm.c
+++ windows_serial_port_comm.c
@@ -7,13 +7,19 @@ int main()
 
 	printf("hello world!\n");
 	int i;
+	char input;
 	char *port = "COM5";
 	DCB dcbSerialParams = {0};
 	COMMTIMEOUTS timeouts = {0};
+
 	unsigned char bytes_received[4];
 	unsigned char bytes_to_send[4];
-	for(i=0; i<64;i++)
-		bytes_to_send[i] = 64-i;
+	//for(i=0; i<64;i++)
+		//bytes_to_send[i] = 64-i;
+	bytes_to_send[0]=1;
+	bytes_to_send[1]=2;
+	bytes_to_send[2]=3;
+	bytes_to_send[3]=4;
 
 	HANDLE hSerial;
     hSerial = CreateFile(
@@ -34,7 +40,7 @@ int main()
         CloseHandle(hSerial);
         return 1;
     }
- 
+
     dcbSerialParams.BaudRate = CBR_9600;
     dcbSerialParams.ByteSize = 8;
     dcbSerialParams.StopBits = ONESTOPBIT;
@@ -61,32 +67,43 @@ int main()
 	DWORD numBytesRead;
 
     numBytesRead = 0;
-
-    while (bytes_received[0] == 0)
+	fprintf(stderr, "bytes received\n");
+    while (1)
     {
       ReadFile(hSerial,           // handle of file to read
                bytes_received,            // store read data here
                sizeof(bytes_received),    // number of bytes to read
                &numBytesRead,           // pointer to number of bytes actually read
                NULL);
-    }
-    //return;
-	fprintf(stderr, "bytes received\n");
 	fprintf(stderr, "%d\t", bytes_received[0]);
 	fprintf(stderr, "%d\t", bytes_received[1]);
 	fprintf(stderr, "%d\t", bytes_received[2]);
 	fprintf(stderr, "%d\t", bytes_received[3]);
+	input = getchar();
+	if(input == 'e')
+		break;
+    }
     
 	// Send specified text (remaining command line arguments)
     DWORD bytes_written, total_bytes_written = 0;
     fprintf(stderr, "\nSending bytes...");
 
+	while(1)
+	{
     if(!WriteFile(hSerial, bytes_to_send, 64, &bytes_written, NULL))
     {
         fprintf(stderr, "Error\n");
         CloseHandle(hSerial);
         return 1;
-    } 
+    }
+	input = getchar();
+	if (input == 'e')
+		break;
+	bytes_to_send[0] = bytes_to_send[0] + 1;
+	bytes_to_send[1] = bytes_to_send[1] + 1;
+	bytes_to_send[2] = bytes_to_send[2] + 1;
+	bytes_to_send[3] = bytes_to_send[3] + 1;
+	}
 
     fprintf(stderr, "%d bytes written\n", bytes_written);
 	
